abc6a34b3e727742895a94ca0871deac7f573d22
Александр Мосолов <144329551+almos05@users.noreply.github.com>
2024-04-17 20:14:04 +0300

    Update lb4.py

--- lb4.py
+++ lb4.py
@@ -5,7 +5,14 @@ with open("test", "r", encoding="utf-8") as file:
 
 cr_A = CreateA('нет да', text)
 cr_A.calculate_tf_and_idf().sort_by_tf_idf().describe_df()
-cr_A.information()
-print(cr_A.real_text)
 
 cr_B = CreateB('нет да', text)
+cr_B.make_tuples()
+
+cr_A.information()
+cr_B.information()
+
+res_docs = cr_A.check_df().sort_index()['TF-IDF'] / cr_B.check_df().iloc[:, 0]
+
+for i in res_docs.sort_values(ascending=False).index:
+    print(cr_A.real_text[i])
